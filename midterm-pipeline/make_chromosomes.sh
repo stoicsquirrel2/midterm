8317885c76c3114175270fbaeefc55141703810f
stoicsquirrel2 <karenoconnor99@gmail.com>
2016-03-17 05:42:35 -0700

    Update make_chromosomes.sh

--- midterm-pipeline/make_chromosomes.sh
+++ midterm-pipeline/make_chromosomes.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if [ $# -eq 0 ]; then
+if [ $# -eq 0 ]; then                               #error check 
     echo "Usage: bash $0 filename.gff output.txt"
     exit 1
 fi
@@ -8,4 +8,4 @@ fi
 filename=$1
 output=$2
 
-cat "$filename" | awk '$3 == "chromosome"' | cut -f 1,4,5 > "$output"
+cat "$filename" | awk '$3 == "chromosome"' | cut -f 1,4,5 > "$output" #prints the chromosome start and end nucleotides
