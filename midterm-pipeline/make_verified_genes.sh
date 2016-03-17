dfb51e9f6921f3a01cfbe53b0ac4469585635486
stoicsquirrel2 <karenoconnor99@gmail.com>
2016-03-17 05:48:28 -0700

    Update make_verified_genes.sh

--- midterm-pipeline/make_verified_genes.sh
+++ midterm-pipeline/make_verified_genes.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if [ $# -eq 0 ]; then
+if [ $# -eq 0 ]; then                                   #error check
     echo "Usage: bash $0 filename.gff output.txt"
     exit 1
 fi
@@ -9,13 +9,13 @@ filename=$1
 output=$2
 
 # NOTE: this is all one big awk statement
-cat "$filename" |
-  awk -v OFS='\t' '$3 == "gene" && match($9,  /orf_classification=Verified$/) { 
-    split($9, array, ";");
+cat "$filename" |               #reads file
+  awk -v OFS='\t' '$3 == "gene" && match($9,  /orf_classification=Verified$/) {  #finds genes whose ORF is verified
+    split($9, array, ";");                                                      #splits column 9 by ; into array
     for(key in array) {
-      if(match(array[key], /^Ontology_term=/)) {
-    	go = substr(array[key], 15)
+      if(match(array[key], /^Ontology_term=/)) {                                #finds ontology_term and
+    	go = substr(array[key], 15)                                             #sets ontology_term to variable go
       }
     };
-    print($1, $4, $5, $7, go);
+    print($1, $4, $5, $7, go);                                                  #outputs chromosome, start, end and GO terms
   }' > "$output"
