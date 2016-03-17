bf1cfcdb6f6c9f8c09504fbf67a9e0283d8c565a
stoicsquirrel2 <karenoconnor99@gmail.com>
2016-03-17 05:31:24 -0700

    Update make_table.sh

--- midterm-pipeline/make_table.sh
+++ midterm-pipeline/make_table.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-if [ $# -eq 0 ]; then
+if [ $# -eq 0 ]; then                                   #check that file has been passed into script
     echo "Usage: bash $0 filename.gff output.txt"
     exit 1
 fi
 
-filename=$1
+filename=$1 #assigns variable name to input and output files
 output=$2
 
-cat "$filename" | sed -e '/##FASTA/,$d' | grep -v '^#' > "$output"
+cat "$filename" | sed -e '/##FASTA/,$d' | grep -v '^#' > "$output" #filters text and removes FASTA info and only prints lines that do not have # at beginning
